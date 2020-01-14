f7b039afe009cb7e41d993fcf5cece8bd3256901
Eden <edendelgado@gmail.com>
2020-01-14 14:40:57 -0600

    file now compiling after name change Bike 01.13

--- src/Bike.hs
+++ src/Bike.hs
@@ -14,4 +14,4 @@ maplist :: (a -> b) -> [a] -> [b]
 maplist functionA2B listA =
   case listA of
     [] -> []
-    (x:xs) -> functionA2B x : maplist xs
+    (x:xs) -> functionA2B x : maplist functionA2B xs
