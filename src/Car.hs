1a912d2a78089f5ab0845014c6c9f51366bfbba7
Eden <edendelgado@gmail.com>
2020-01-15 11:25:02 -0600

    create mapList and foldl

--- src/Car.hs
+++ src/Car.hs
@@ -10,3 +10,24 @@ data Car = Car
 newtype CarNickName = CarNickName T.Text
 
 newtype CarMiles = CarMiles Int
+
+mapList :: (a -> b) -> [a] -> [b]
+mapList funcationA2B listA =
+  case listA of
+    [] -> []
+    (x:xs) -> funcationA2B x : mapList funcationA2B xs
+
+mapListTwo :: (a -> b) -> [a] -> [b]
+mapListTwo funcationA2B [] = []
+mapListTwo funcationA2B (x:xs) = funcationA2B x : mapListTwo funcationA2B xs
+
+makeFoldl :: (b -> a -> b) -> b -> [a] -> b
+makeFoldl funcationBA2B b listA =
+  case listA of
+    [] -> b
+    (x:xs) -> makeFoldl funcationBA2B (funcationBA2B b x) xs
+
+makeFoldlTwo :: (b -> a -> b) -> b -> [a] -> b
+makeFoldlTwo functionBA2B b [] = b
+makeFoldlTwo functionBA2B b (x:xs) =
+  makeFoldlTwo functionBA2B (functionBA2B b x) xs
