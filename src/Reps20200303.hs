a8da681d0d4d5a6f2c6b02c762087b8e9f6a97e0
Eden <edendelgado@gmail.com>
2020-03-04 09:46:13 -0600

    Fin 4 reps

---
+++ src/Reps20200303.hs
@@ -0,0 +1,173 @@
+module Reps20200303 () where
+
+joinList :: [[a]] -> [a]
+joinList listOFlist =
+  case listOFlist of
+    [] -> []
+    (list:restOfLists) ->
+      list <> joinList restOfLists
+
+joinMaybe :: Maybe (Maybe a) -> Maybe a
+joinMaybe mMa =
+  case mMa of
+    Nothing -> Nothing
+    Just ma -> ma
+
+bindList :: (a -> [b]) -> [a] -> [b]
+bindList functionA2ListB lista =
+  case lista of
+    [] -> []
+    (x:xs) ->
+      functionA2ListB x <> bindList functionA2ListB xs
+
+bindMaybe :: (a -> Maybe b) -> Maybe a -> Maybe b
+bindMaybe functionA2ListB ma =
+  case ma of
+    Nothing -> Nothing
+    Just a ->
+      functionA2ListB a
+
+--joinListWBind :: [[a]] -> [a]
+--joinListWBind listOFlist =
+--  case listOFlist of
+--    [] -> []
+--    (list:restofLists) ->
+
+
+{-
+Create a new type around List
+Implement Applicative for it
+Provide a pure and apply instance
+Make pure be correct for applyZip
+
+Understand "->" in types
+figure out the Type for Fmap, Pure, (<*>)
+figure out the method for Fmap, Pure, (<*>)
+
+Monad
+join       :: Monad m => m (m a) -> m a
+Bind (>>=) :: m a -> (a -> m b) -> m b
+"I'd specifically like you to write bind with the arguments flipped,
+  so that it mirrors Functor and Applicative"
+
+Write join for List
+Write join for Maybe
+Write Bind for List
+Write Bind for Maybe
+
+Write join for list useing Bind
+Write join for Maybe useing Bind
+Write Bind for list useing join
+Write Bind for Maybe useing join
+
+## Reps
+
+### Create a ...
+* Model in a new file
+* -data record-
+* -Two different new type-
+* -write a function that will create an instance of your record that you just created.-
+* -write a function that will edit part of your record.-
+
+### Write the function out
+* -function that does what fmap does for a list-
+* -function that does what fmap does for a Maybe-
+* -function that does what foldl-
+
+### Data types
+##### Enumerated (enum) type
+-Enum is a data type consisting of a set of named values.-
+* -Create a data that has three different Color constructors-
+* -Create a function that goes from your Color to a String of- your color.
+  * -if you give Blue it gives you a Spring that says "Blue"-
+
+##### Basic Algebraic Data Type
+-An algebraic data type (ADT) has one or more data constructors,-
+-and each data constructor can have zero or more arguments.-
+* -Create data Vegetable = Celery | Carrot Color-
+* -Create a function that takes a Vegetable and gives a Color-
+	* -vegetableColor :: Vegetable -> Color-
+* -Create a function that takes a Vegetable and give a String of the color-
+	* -vegetableToString :: Vegetable -> String-
+
+##### Polymorphic Algebraic data type
+  * -Create a data called Keep that has two constructors =Shoebox a | Safe a-
+  * -Create function takeOut :: Keep a -> a-
+	* -Put a Red Carrot into a ShoeBox(Keep) and write out what its type would be-
+	* -Create a ShoeBox Carrot and write out what its type would be-
+
+##### Create your own function
+* Make Maybe
+	* implement map for your Maybe
+  * create Functor instance for your Maybe
+
+* Make List
+	* implement map for your List
+  * create Functor instance for your List
+
+##### Apply / Pure
+* pureMaybe :: a -> Maybe a
+
+* pureList :: a -> [a]
+
+* members :: Maybe [String]
+
+* applyMaybe ::
+
+* applyList ::
+
+* applyZipList ::
+
+* lengthOfEachMeber :: Maybe [String] -> Maybe [Int]
+  * Should work like
+  * Just ["Bob", "Carol"] -> Just [3,5]
+* write with your functions and then fmap BOTH
+
+* `~~~~` :: [Maybe String]
+
+* lengthOfEachMeberTwo :: [Maybe String] -> [Maybe Int]
+* write with your functions and then fmap BOTH
+
+##### Pre applicative
+
+* Create a data that is a String
+* Create a :: Maybe String
+* Use fmap to create an instance of that data type using the maybe value.
+* Create a data type that needs a String and Int
+* Craete a :: Maybe Int
+* Use fmap and apply to create an instance of that data type (String Int) using the the two Maybe values
+
+use ApplyList
+ newRepFunction :: List EdenStringIntInt
+ newRepFunction =
+
+Once you better understand using applyList
+start using infix notation for AppleList and applyMaybe ex= `functionName`
+
+createInstance :: Maybe EdenString
+Write something with fmap "create insatnce" that only useing fmap.
+-- Now right that with out fmap. Use PureMaybe and applyMaybe maybe.
+-- Now that you wrote one for Myabe write one for List
+
+Create applicative instance for MakeMaybe and MakeList FOR pure and (<*>)
+
+instance Applicative MakeList where
+  pure =
+  (<*>) =
+
+* Rewrite the functions you wrote with infix notation to use operators
+* Create code behind the operator (.) Function composition.
+  * Create code behind the operator (.) Function composition.
+  * write (.) four different times, with 0,1,2,3 Lambdas
+  and change "(" in the type to respent the changes
+  * write wiht \a b c ->
+
+* Build IO Int that gives the length of a string that is typed into getLine.
+* Use the functor instance of IO
+
+* build a data Record with two string fields. Use applicative syntax that will build an IO Record^ by asking for each field to be entered as a line.
+
+* ^^ do both in the REPL.
+
+
+-}
