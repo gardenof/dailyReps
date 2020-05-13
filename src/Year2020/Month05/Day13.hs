e30d9532d23e505d2e1092d84886e3fdcb084613
Eden <edendelgado@gmail.com>
2020-05-13 15:22:39 -0500

    fin reps 2020.05.13

---
+++ src/Year2020/Month05/Day13.hs
@@ -0,0 +1,238 @@
+module Year2020.Month05.Day13 () where
+
+import qualified Data.Char as Char
+
+data Car = Car
+  { carName :: String
+  , carYear :: Int
+  }
+
+createCarRec :: String -> Int -> Car
+createCarRec  nameStr yearInt =
+  Car { carName = nameStr
+      , carYear = yearInt
+      }
+
+carConstructor :: String -> Int -> Car
+carConstructor =
+  Car
+
+carConstructorReg :: String -> Int -> Car
+carConstructorReg nameStr yearInt =
+  Car nameStr yearInt
+
+updateName :: String -> Car -> Car
+updateName newNameStr car =
+  car { carName = newNameStr }
+
+{-
+Basics of Haskell Records
+- Create a record with two fields of different types
+- write a function that will create an instance of your record using record syntax
+- write down the type of the data constructor for your record
+- write a function that will create an instance of your record using regular data constructor syntax
+- write a function that will update one of the fields of an existing record with a new value
+-}
+
+data Color
+  = Green
+  | Blue
+  | Red
+
+colorToString :: Color -> String
+colorToString col =
+  case col of
+    Green -> "green"
+    Blue -> "blue"
+    Red -> "red"
+
+data Vegetable
+  = Celery
+  | Carrot Color
+
+vegToColor :: Vegetable -> Color
+vegToColor veg =
+  case veg of
+    Celery -> Green
+    Carrot col -> col
+
+vegToString :: Vegetable -> String
+vegToString veg =
+  colorToString $ vegToColor veg
+
+data Keep a
+  = Shoebox a
+  | Safe a
+
+insideKepp :: Keep a -> a
+insideKepp keep =
+  case keep of
+    Shoebox a -> a
+    Safe a -> a
+
+redCarrotInShoebox :: Keep Vegetable
+redCarrotInShoebox =
+  Shoebox $ Carrot Red
+
+carrotInShoebox :: Keep (Color -> Vegetable)
+carrotInShoebox =
+  Shoebox Carrot
+
+
+{-
+Enumerated (enum) type
+  Enum is a data type consisting of a set of named values.
+  - Create a data that has three different Color constructors
+  - Create a function that goes from your Color to a String of your color.
+Basic Algebraic Data Type
+  An algebraic data type (ADT) has one or more data constructors,
+  and each data constructor can have zero or more arguments.
+  - Create type to represent Vegetables. Celery has data arguments. Carrots
+    need a color.
+  - Create a function that takes a Vegetable and gives its Color.
+  - Create a function that takes a Vegetable and give a String of the color.
+Polymorphic Algebraic data type
+  - Create a type called Keep that can story anything. Things can be kept in
+    either a Shoebox or a Safe.
+  - Create a function to take items out of the Keep.
+  - Put a Red Carrot into a Shoebox(Keep) and write out what its type would be.
+  - Create a Shoebox Carrot and write out what its type would be.
+-}
+
+{-
+Reflexivity :: x == x True
+
+Symmetry :: x == y = y == x
+
+Transitivity :: x == y and y == z then x == z
+
+Negation x /= y = not ( x == y )
+-}
+
+data Pie
+  = Apple
+  | Pumpkin
+  | Rhubarb
+
+instance Eq Pie where
+  left == right =
+    case (left,right) of
+      (Apple,Apple) -> True
+      (Pumpkin,Pumpkin) -> True
+      (Rhubarb,Rhubarb) -> True
+      _ -> False
+
+data RecipeIngredient =
+  RecipeIngredient
+    { recName :: String
+    , recFeeds :: Int
+    }
+
+instance Eq RecipeIngredient where
+  left == right =
+    recName left == recName right
+    && recFeeds left == recFeeds right
+
+data Drink
+  = Water
+  | Soda
+  | Juice
+  deriving Eq
+
+data Meal = Meal
+  { meanName :: String
+  , mealFeeds :: Int
+  }deriving Eq
+
+{-
+Eq
+  | Eq is a typeclass for types that can be compared for equality.
+- Write out the Eq "Laws"
+  * Note that the Haskell Report that defines the official Haskell language
+    does not give an official laws for Eq. Despite this, there are customary
+    laws given in the docs for Eq that represent the general expected practice
+    for implementing Eq. The laws given in the answer here are those, with
+    one omission (Substitutivity), for simplicities sake.
+  - Reflexivity
+  - Symmetry
+  - Transitivity
+  - Negation
+- Define an enum type and provide an Eq instance for it
+- Define a record type and provide an Eq instance for it
+- Define another enum type and derive Eq for it
+- Define another record type and derive Eq for it
+-}
+
+newtype Name = Name String
+
+nameToString :: Name -> String
+nameToString (Name nameStr) =
+  nameStr
+
+newtype Hometown = Hometown String
+
+hometownToString :: Hometown -> String
+hometownToString (Hometown homeTownStr)=
+  homeTownStr
+
+newtype Age = Age Int
+
+data Person = Person
+  { personName :: Name
+  , personHomeTown :: Hometown
+  , personAge :: Age
+  }
+
+personOne :: Person
+personOne =
+  Person
+    { personName = Name "jim"
+    , personHomeTown = Hometown "Zone8"
+    , personAge = Age 8
+    }
+
+introduction :: Person -> String
+introduction person =
+  "Hello"
+  ++ (nameToString (personName person))
+  ++ ", from"
+  ++ (hometownToString (personHomeTown person))
+
+instance Eq Hometown where
+  left == right =
+    hometownToString left == hometownToString right
+
+instance Eq Name where
+  left == right =
+    let
+      nameleft = map Char.toLower (nameToString left)
+      nameright = map Char.toLower (nameToString right)
+    in
+      nameleft == nameright
+
+{--
+  Newtypes
+    | A "newtype" is a type defined with tha `newtype` keyword. It is a
+    | trivial wrapper around an existing type that creates a brand new type
+    | out of it that the compiler will treat as different from the original
+    | type. This is useful for many things, including distinguishing between
+    | values that would otherwise be the same simple type. The compiler
+    | removes newtype wrappers at compile time so that there is littre to
+    | no runtime cost associated with having many newtypes.
+- Define a type using newtype to represent a person's Name
+- Implement a function nameToString to convert a Name to String
+- Define a type using newtype to represent a person's Hometown
+- Implement a function hometownToString to convert a Hometown to String
+- Define a type using newtype to represent a person's Age
+- Define a Person record using these three types.
+- Construct a value of type Person.
+- Implement a function to create a String introducing a Person by stating their
+  name and where they are from. Be polite and omit their age from the
+  introduction.
+| Newtypes can also be used create a wrapper for the purpose of providing
+| a new typeclass instance for a type that already has one. We will use this
+| technique in these reps occasionally to allow us to re-write the typeclass
+| implementations in common libraries as a way to understand them better.
+- define an Eq instance for Hometown that is equivalent to the one for String
+- define an Eq instance for Name that compares names in a case-insensitive manner.
+--}
