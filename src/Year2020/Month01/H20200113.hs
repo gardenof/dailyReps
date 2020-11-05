5fbb8115f4785b41ba09312586b577ccf9fcdac6
Eden <edendelgado@gmail.com>
2020-11-05 11:24:44 -0600

    WIP

--- src/Year2020/Month01/H20200113.hs
+++ src/Year2020/Month01/H20200113.hs
@@ -1,15 +1,5 @@
 module Year2020.Month01.H20200113 () where
 
-import qualified  Data.Text as T
-
-data Bike = Bike { nickName :: NickName
-                 , odometer :: Odometer
-                 }
-
-newtype NickName = NickName T.Text
-
-newtype Odometer = Odometer Int
-
 maplist :: (a -> b) -> [a] -> [b]
 maplist functionA2B listA =
   case listA of
